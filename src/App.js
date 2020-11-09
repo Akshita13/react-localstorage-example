4147ec6052cf6ebed4228f81fe82aa1f2b9197e7
Akshita13 <akshitakapadia13@gmail.com>
2020-11-09 16:04:21 +0530

    1 file modified

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import "./style.css";
 
 export default function App() {
-  const [wordsToStorage, setWordsToStorage] = React.useState("abc");
+  const [wordsToStorage, setWordsToStorage] = React.useState("");
   const onhandleChange = value => {
     localStorage.setItem("name", wordsToStorage);
     setWordsToStorage(value);
@@ -11,14 +11,20 @@ export default function App() {
     localStorage.clear();
     setWordsToStorage("");
   };
+
   return (
     <div>
       <h1>Hello StackBlitz!</h1>
-      <input type="text" onChange={e => onhandleChange(e.target.value)} value={wordsToStorage}/>
+      <input
+        type="text"
+        onChange={e => onhandleChange(e.target.value)}
+        value={wordsToStorage}
+        placeholder={!wordsToStorage && "please enter value to store"}
+      />
       {localStorage.getItem("name") ? (
         <>
-        <h1>{wordsToStorage}</h1>
-        <button onClick={() => onClear()}>Clear</button>
+          <h1>{wordsToStorage}</h1>
+          <button onClick={() => onClear()}>Clear from storage</button>
         </>
       ) : (
         <h1>{wordsToStorage}</h1>
